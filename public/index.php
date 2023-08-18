166d554076ec48ba7264d35aa4a82fd9ee73d4a1
Rybensx <anderson_arf@hotmail.es>
2023-08-17 23:17:32 -0600

    ssss

--- public/index.php
+++ public/index.php
@@ -1,15 +1,18 @@
 <?php 
-require_once __DIR__ . '/../includes/app.php';
+require_once _DIR_ . '/../includes/app.php';
 
 
 use MVC\Router;
 use Controllers\AppController;
 use Controllers\LoginController;
+use Controllers\MenuController;
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
 $router->get('/', [LoginController::class,'index']);
 $router->post('/API/login', [LoginController::class,'loginAPI']);
+$router->get('/menu', [MenuController::class,'index']);
+$router->get('/cerrarsesion', [MenuController::class,'cerrarsesion']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
---
+++ views/menu/index.php
@@ -0,0 +1,34 @@
+<?php if(isset($_SESSION['auth_user'])){?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="build/js/app.js"></script>
+    <link rel="shortcut icon" href="<?= asset('images/cit.png') ?>" type="image/x-icon">
+    <link rel="stylesheet" href="<?= asset('build/styles.css') ?>">
+    <title>Menú</title>
+</head>
+<body class="bg-image bg-opacity-50">
+    <div class="bg-light bg-opacity-10 w-100" style="height: 100vh;">
+        <div class="container-fluid pt-5 mb-4">
+            <h2 class="text-center mb-4 text-primary">Menú</h2>
+            
+            <div class="d-grid">
+                <button  class="btn btn-primary"  id="cerrarsesion">Cerrar Sesión</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<?php }
+
+else{
+
+    header("Location: /login");
+}
+
+?>
+
+<script src="<?= asset('./build/js/menu/index.js') ?>"></script>
